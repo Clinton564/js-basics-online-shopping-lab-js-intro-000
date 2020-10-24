5af67210fffd802fb219671c7ae474dd89e1774c
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 15:31:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
+    total=total();
     cart.splice(0)
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
