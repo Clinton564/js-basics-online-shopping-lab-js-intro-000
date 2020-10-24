a4ff5ea479e4772900e89106f79af72c541713d7
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 15:21:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     return console.log(`${cart[i].itemName} == ${item}`)
     if(cart[i].itemName==item) {
-      cart.splice(i, 1)
+      cart.splice(1, 1)
     }
     break
   }
