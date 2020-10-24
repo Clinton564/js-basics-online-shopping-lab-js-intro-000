052ea578d79e78273e991830f547c8f1e842f838
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 15:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,11 +62,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  //let total = total();
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    total=total()
+    let total=total()
     cart.splice(0)
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
