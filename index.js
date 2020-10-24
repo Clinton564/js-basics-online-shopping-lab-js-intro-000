082f026e78fc772f8e57b54f12861a1e5e06eebc
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 15:35:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-    let bill=`$${total()}`
+    let bill=total()
     cart.splice(0)
     return `Your total cost is $${bill}, which will be charged to the card ${cardNumber}.`
   }
