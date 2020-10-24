846bd36493624bf64a0fa72f669d622e96be42ee
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-24 14:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-cart.push({itemName: item, itemPrice: Math.floor(Math.random() * Math.floor(max))})
+cart.push({itemName: item, itemPrice: Math.floor(Math.random() * Math.floor(100))})
 }
 
 function viewCart() {
